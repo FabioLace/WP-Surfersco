ce9af792409545ed5e1a60e2f664f589204d0dba
fabio.lacerra <fabio.lx26@gmail.com>
2024-01-22 14:08:20 +0100

    feat: master: remevd gutenberg and adapted files frm original demo-surfersco project.

--- web/app/themes/surfersco/functions.php
+++ web/app/themes/surfersco/functions.php
@@ -63,3 +63,13 @@ collect(['setup', 'filters'])
             );
         }
     });
+
+// DISABLE GUTENBERG
+add_filter( 'use_block_editor_for_post', '__return_false' );
+add_filter( 'use_widgets_block_editor', '__return_false' );
+
+add_action( 'wp_enqueue_scripts', function() {
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style( 'global-styles' );
+}, 20 );
